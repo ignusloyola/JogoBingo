4c100b2acc124b8ca9bb1bfb00b1a15e04fd5816
inacio <inacio@inacio-PC>
2015-05-27 00:19:16 -0300

    incersao parcial das listas de nomes e seus significados

--- Inicial/src/classes/sorteio.java
+++ Inicial/src/classes/sorteio.java
@@ -14,17 +14,152 @@ import java.util.ArrayList;
 public class sorteio {
     
     ArrayList<String> dicionario = new ArrayList<String>();
+    ArrayList<String> discricao = new ArrayList<String>();
     
     public void CarregarPalavras()
     {
-        dicionario.add("casa");
-        dicionario.add("terra");
-        dicionario.add("xadrez");
-        dicionario.add("chuva");
-        dicionario.add("muleque");
-        dicionario.add("homem");
+        dicionario.add(1,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        dicionario.add(2,"terra");
+        dicionario.add(3,"xadrez");
+        dicionario.add(4,"chuva");
+        dicionario.add(5,"muleque");
+        dicionario.add(6,"homem");
+        
+        dicionario.add(7,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        dicionario.add(8,"terra");
+        dicionario.add(9,"xadrez");
+        dicionario.add(10,"chuva");
+        dicionario.add(11,"muleque");
+        dicionario.add(12,"homem");
+        
+        dicionario.add(13,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        dicionario.add(14,"terra");
+        dicionario.add(15,"xadrez");
+        dicionario.add(16,"chuva");
+        dicionario.add(17,"muleque");
+        dicionario.add(18,"homem");
+        
+        dicionario.add(19,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        dicionario.add(20,"terra");
+        dicionario.add(21,"xadrez");
+        dicionario.add(22,"chuva");
+        dicionario.add(23,"muleque");
+        dicionario.add(24,"homem");
+        
+        dicionario.add(25,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        dicionario.add(26,"terra");
+        dicionario.add(27,"xadrez");
+        dicionario.add(28,"chuva");
+        dicionario.add(29,"muleque");
+        dicionario.add(30,"homem");
+        
+        dicionario.add(31,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        dicionario.add(32,"terra");
+        dicionario.add(33,"xadrez");
+        dicionario.add(34,"chuva");
+        dicionario.add(35,"muleque");
+        dicionario.add(36,"homem");
+        
+        dicionario.add(37,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        dicionario.add(38,"terra");
+        dicionario.add(39,"xadrez");
+        dicionario.add(40,"chuva");
+        dicionario.add(41,"muleque");
+        dicionario.add(42,"homem");
+        
+        dicionario.add(43,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        dicionario.add(44,"terra");
+        dicionario.add(45,"xadrez");
+        dicionario.add(46,"chuva");
+        dicionario.add(47,"muleque");
+        dicionario.add(48,"homem");
+        
+        dicionario.add(49,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        dicionario.add(50,"terra");
+        dicionario.add(51,"xadrez");
+        dicionario.add(52,"chuva");
+        dicionario.add(53,"muleque");
+        dicionario.add(54,"homem");
+        
+        dicionario.add(55,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        dicionario.add(56,"terra");
+        dicionario.add(57,"xadrez");
+        dicionario.add(58,"chuva");
+        dicionario.add(59,"muleque");
+        dicionario.add(60,"homem");
     }
-    
+    public void CarregarDescricao()
+    {
+        discricao.add(1,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        discricao.add(2,"terra");
+        discricao.add(3,"xadrez");
+        discricao.add(4,"chuva");
+        discricao.add(5,"muleque");
+        discricao.add(6,"homem");
+        
+        discricao.add(7,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        discricao.add(8,"terra");
+        discricao.add(9,"xadrez");
+        discricao.add(10,"chuva");
+        discricao.add(11,"muleque");
+        discricao.add(12,"homem");
+        
+        discricao.add(13,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        discricao.add(14,"terra");
+        discricao.add(15,"xadrez");
+        discricao.add(16,"chuva");
+        discricao.add(17,"muleque");
+        discricao.add(18,"homem");
+        
+        discricao.add(19,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        discricao.add(20,"terra");
+        discricao.add(21,"xadrez");
+        discricao.add(22,"chuva");
+        discricao.add(23,"muleque");
+        discricao.add(24,"homem");
+        
+        discricao.add(25,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        discricao.add(26,"terra");
+        discricao.add(27,"xadrez");
+        discricao.add(28,"chuva");
+        discricao.add(29,"muleque");
+        discricao.add(30,"homem");
+        
+        discricao.add(31,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        discricao.add(32,"terra");
+        discricao.add(33,"xadrez");
+        discricao.add(34,"chuva");
+        discricao.add(35,"muleque");
+        discricao.add(36,"homem");
+        
+        discricao.add(37,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        discricao.add(38,"terra");
+        discricao.add(39,"xadrez");
+        discricao.add(40,"chuva");
+        discricao.add(41,"muleque");
+        discricao.add(42,"homem");
+        
+        discricao.add(43,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        discricao.add(44,"terra");
+        discricao.add(45,"xadrez");
+        discricao.add(46,"chuva");
+        discricao.add(47,"muleque");
+        discricao.add(48,"homem");
+        
+        discricao.add(49,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        discricao.add(50,"terra");
+        discricao.add(51,"xadrez");
+        discricao.add(52,"chuva");
+        discricao.add(53,"muleque");
+        discricao.add(54,"homem");
+        
+        discricao.add(55,"Coisa trabalhosa, complicada, embrulhada, intrincada.");
+        discricao.add(56,"terra");
+        discricao.add(57,"xadrez");
+        discricao.add(58,"chuva");
+        discricao.add(59,"muleque");
+        discricao.add(60,"homem");
+    }
     public void sorteio(int qtd, ArrayList list)
     {
         CarregarPalavras();
